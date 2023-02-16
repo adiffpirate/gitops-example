88ef1c030059dc11df193e484314137445f212a6
adiffpirate <lpsmonteiro@gmail.com>
2023-02-16 16:50:46 -0300

    Added Prometheus application on ArgoCD

--- ops/tf_modules/argocd/applications.tf
+++ ops/tf_modules/argocd/applications.tf
@@ -1,3 +1,7 @@
+#------#
+# APPS #
+#------#
+
 resource "kubectl_manifest" "application" {
   for_each   = toset(var.applications)
   depends_on = [kubectl_manifest.argocd]
@@ -30,6 +34,10 @@ resource "kubectl_manifest" "application" {
   YAML
 }
 
+#--------------------------#
+# NGINX Ingress Controller #
+#--------------------------#
+
 resource "kubectl_manifest" "application_nginx_ingress_controller" {
   depends_on = [kubectl_manifest.argocd]
 
@@ -60,6 +68,71 @@ resource "kubectl_manifest" "application_nginx_ingress_controller" {
       syncPolicy:
         syncOptions:
           - CreateNamespace=true
+        automated:
+          selfHeal: true
+  YAML
+}
+
+#-------------------------#
+# Prometheus with Grafana #
+#-------------------------#
+
+resource "kubectl_manifest" "application_prometheus" {
+  depends_on = [kubectl_manifest.argocd]
+
+  yaml_body = <<-YAML
+    apiVersion: argoproj.io/v1alpha1
+    kind: Application
+    metadata:
+      name: prometheus
+      namespace: argocd
+    spec:
+      project: default
+
+      source:
+        repoURL: https://prometheus-community.github.io/helm-charts
+        chart: kube-prometheus-stack
+        targetRevision: 45.1.1
+        helm:
+          skipCrds: true
+      destination:
+        server: https://kubernetes.default.svc
+        namespace: prometheus
+
+      syncPolicy:
+        syncOptions:
+          - CreateNamespace=true
+        automated:
+          selfHeal: true
+  YAML
+}
+
+resource "kubectl_manifest" "application_prometheus_crds" {
+  depends_on = [kubectl_manifest.argocd]
+
+  yaml_body = <<-YAML
+    apiVersion: argoproj.io/v1alpha1
+    kind: Application
+    metadata:
+      name: prometheus-crds
+      namespace: argocd
+    spec:
+      project: default
+
+      source:
+        repoURL: https://github.com/prometheus-community/helm-charts
+        path: charts/kube-prometheus-stack/crds
+        targetRevision: kube-prometheus-stack-45.1.1
+        directory:
+          recurse: true
+      destination:
+        server: https://kubernetes.default.svc
+        namespace: prometheus
+
+      syncPolicy:
+        syncOptions:
+          - CreateNamespace=true
+          - Replace=true
         automated:
           selfHeal: true
   YAML
